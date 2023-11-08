7ec9edf5e52cc03bd664add2255c3baf0a0a8b63
opaeshuy <oliver.paeshuys@ugent.be>
2023-11-08 14:13:20 +0100

    Aanpassingen FiniteDiff

--- FiniteDifferences.py
+++ FiniteDifferences.py
@@ -19,30 +19,36 @@ class FiniteDifferences:
     def __init__(self,Grid):
         
         #Schemes for first-order (D) and second order ( DD) derivatives
-        self.CentralStencilD=np.array([-1.0,0.0,1.0])/(2.0*Grid.dx)
-        self.BackwardStencilD=
-        self.ForwardStencilD=
+        self.CentralStencilD=np.array([-1.0,1.0,0.0])/(2.0*Grid.dx)
+        self.BackwardStencilD=np.array([-1.0,1.0,0.0])/(1.0*Grid.dx)
+        self.ForwardStencilD=np.array([-1.0,1.0,0])/(1.0*Grid.dx)
         
-        self.CentralStencilDD=
+        self.CentralStencilDD=np.array([1.0,-2.0,1.0])/(Grid.dx**2)
+        self.BackwardStencilDD=np.array([1.0,-2.0,1.0])/(Grid.dx**2)
+        self.ForwardStencilDD=np.array([1.0,-2.0,1.0])/(Grid.dx**2)
 
         
         # Sparse Matrix Operators
         self.Identity=sparse.identity(Grid.Nx, dtype='float', format="csr")
         
-        self.DDXCentral=sparse.diags(self.CentralStencilD, [-1, 0, 1], shape=(Grid.Nx, Grid.Nx), dtype='float', format="csr")
+        self.DDXCentral=sparse.diags(self.CentralStencilD, [-1, 1, 0], shape=(Grid.Nx, Grid.Nx), dtype='float', format="csr")
         self.DDXCentral #Define right boundary stencil
         self.DDXCentral #Define left boundary stencil
 
-        self.DDXBackward=
+        self.DDXBackward=sparse.diags(self.BackwardStencilD, [-1, 0, 1], shape=(Grid.Nx, Grid.Nx), dtype='float', format="csr")
         self.DDXBackward #Define boundary stencil
 
-        self.DDXForward=
-        self.DDXForward  #Define boundary stencil
+        self.DDXForward=sparse.diags(self.ForwardStencilD, [0,1,-1], shape=(Grid.Nx, Grid.Nx), dtype='float', format="csr")
+        self.DDXForward #Define boundary stencil
+
+
+        self.D2DX2Central = sparse.diags(self.CentralStencilDD, [-1,0,1] , shape=(Grid.Nx, Grid.Nx), dtype='float', format="csr")
         
-        self.D2DX2=
-        self.D2DX2 #Define right boundary stencil
-        self.D2DX2 #Define left boundary stencil
-        
+        self.D2DX2Backward = sparse.diags(self.ForwardStencilDD,[0,-1,-2] , shape=(Grid.Nx, Grid.Nx), dtype='float', format="csr")
+
+        self.D2DX2Forward= sparse.diags(self.ForwardStencilDD,[0,1,2] , shape=(Grid.Nx, Grid.Nx), dtype='float', format="csr")
+        self.D2DX2Forward #Define right boundary stencil
+        self.D2DX2Forward #Define left boundary stencil
 
     # Efficient Implementation for 1D csr type FD matrix
     #do not Change implementation below!
