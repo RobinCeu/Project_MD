d10edd6d9c1cb2f7aeecf4fde1404409b58abf32
RobinCeu <you@example.com>
2023-11-24 11:24:30 +0100

    push werk van 23_11

--- test.py
+++ test.py
@@ -1,23 +1,25 @@
 import numpy as np
-
-"hallo"
-
-"hallo"
-
-import numpy as np
 from scipy.interpolate import interp1d
 from scipy.integrate import quad
 
-# Suppose you have x values and corresponding y values in arrays
-y_values = press
-x_values = Grid.x
+def integrate_discrete_data(x_values, y_values):
+    # Create an interpolation function
+    interp_func = interp1d(x_values, y_values, kind='linear', fill_value='extrapolate')
 
-# Create an interpolating function
-interp_func = interp1d(x_values, y_values, kind='linear', fill_value='extrapolate')
+    # Define the integration function
+    def integrand(x):
+        return interp_func(x)
 
-# Define the function to integrate
-def integrand(x):
-    return interp_func(x)
+    # Integrate the function using quad
+    result, error = quad(integrand, x_values[0], x_values[-1])
 
-# Integrate the function using quad
-W_hyd, W_hyd_error = quad(integrand, x_values[0], x_values[-1])
+    return result, error
+
+# Example usage
+x_values = np.array([0, 1, 2, 3, 4])
+y_values = np.array([1, 4, 6, 8, 10])
+
+result, error = integrate_discrete_data(x_values, y_values)
+
+print("Result of integration:", result)
+print("Estimated error:", error)
